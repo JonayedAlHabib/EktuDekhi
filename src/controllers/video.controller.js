e3f2f0a65b1de5c14f7bba40d24abe4d8eec2bac
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-02 03:48:58 +0600

    feat: implement uploadVideo controller with file validation and cloud upload

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,6 +3,8 @@ import {Video} from "../models/video.model.js"
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { User } from "../models/user.model.js";
 
 const getAllVideos = asyncHandler (async (req, res) =>{
     const {page=1, limit=10, query, sortBy, sortType, userId} = req.query
@@ -43,6 +45,43 @@ const getAllVideos = asyncHandler (async (req, res) =>{
 })
 
 const uploadVideo = asyncHandler (async (req, res) =>{
+    const {title, description, duration} = req.body
+
+    if(!title || !description || !duration)
+        throw new ApiError(400, "Title, description and duration are required")
+
+    const videoLocalPath = req.files?.videoFile?.[0]?.path
+    const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path
+
+    if(!videoLocalPath)
+        throw new ApiError(400, "Video file is required")
+    
+    if(!thumbnailLocalPath)
+        throw new ApiError(400, "Thumbnail is required")
+
+    const videoFile = await uploadOnCloudinary(videoLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if(!videoFile?.url)
+        throw new ApiError(400, "Error while uploading video file")
+    
+    if(!thumbnail?.url)
+        throw new ApiError(400, "Error while uploading thumbnail")
+
+    const video = await Video.create({
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        owner: req.user._id,
+        title,
+        description,
+        duration: parseInt(duration)
+    })
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201, video, "Video uploaded successfully")
+    )
 
 })
 
