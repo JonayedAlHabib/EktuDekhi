f5d45c7a90fef946361046edf2b8b79a5f01f497
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-02 01:16:47 +0600

    feat: define comment schema with content, video, and owner fields

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -2,7 +2,20 @@ import mongoose from "mongoose"
 import mongoosePaginate from "mongoose-paginate-v2"
 
 const commentSchema = new mongoose.Schema({
-    
-})
+    content: {
+        type: String,
+        required: true
+    },
+    video:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps: true})
+
+commentSchema.plugin("mongoosePaginate")
 
 export const Comment = mongoose.model("Comment", commentSchema)
