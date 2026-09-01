a6ccd09d46dec1b74e7fd652747c2b9cf9cb4f50
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-01 19:41:21 +0600

    feat: add getCurrentUser functionality to retrieve current user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -209,6 +209,18 @@ const changePassword = asyncHandler (async (req, res) =>{
 
 })
 
+const getCurrentUser = asyncHandler (async (req, res) =>{
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "User Fetched Successfully"
+        )
+    )
+})
+
 export {
     registerUser,
     loginUser,
