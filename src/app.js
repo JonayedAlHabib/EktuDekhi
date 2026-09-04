accb4fbcb30890ebfc0c9d78f940dc0c9b15fda4
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-04 23:14:09 +0600

    feat: add playlist router to app for handling playlist-related requests

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import subscribeRouter from "./routes/subscribe.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 
 // router declaration
@@ -33,6 +34,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/subscriptions", subscribeRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/playlists", playlistRouter)
 
 // http://localhost:4000/api/v1/users
 
