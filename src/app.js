0135def54951b1f93e509faeb938df1f8a67ccbe
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-04 22:11:24 +0600

    feat: add tweet router to app for handling tweet-related requests

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import subscribeRouter from "./routes/subscribe.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
 
 
 // router declaration
@@ -31,6 +32,7 @@ app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/subscriptions", subscribeRouter)
+app.use("/api/v1/tweets", tweetRouter)
 
 // http://localhost:4000/api/v1/users
 
